4f3723d9473fcd6421b6d0533f421d35afe3666c
devraj2018 <shekhawatdevraj123@gmail.com>
2021-10-20 00:10:46 +0530

    Update

---
+++ 150-Evaluate Reverse Polish Notation.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isOperator(string x)
+    {
+         if(x=="+" || x=="/" || x=="*" || x=="-")
+             return true;
+        return false;
+     }
+    int evalRPN(vector<string>& tokens) {
+        int n=tokens.size();
+        stack<int> s;
+        for(int i=0;i<n;i++)
+           {  string x=tokens[i];
+                if(!isOperator(x))
+                    {s.push(stoi(x));
+                       continue;
+                    }
+                int b= s.top();
+                  s.pop();
+                int a=s.top();
+                  s.pop();
+                if(x=="+") s.push(a+b);
+                else if(x=="-") s.push(a-b);
+                else if(x=="*") s.push(a*b);
+                else s.push(a/b);
+             
+         }
+        return s.top();
+       
+    }
+};
